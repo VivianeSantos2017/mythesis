9a4fb28fba60b5d1693eb88f09e499fe3afb082f
Viviane <vivianesantos@jbrj.gov.br>
2019-12-11 18:36:58 +0800

    Incluidos testes BC

--- Scripts/08_PCA.R
+++ Scripts/08_PCA.R
@@ -266,3 +266,107 @@ plot.PCA(pca.sel.X.CV, axes=c(1, 2), choix = "ind", habillage=1, label = "var", 
 fviz_pca_biplot(pca.sel.X.CV,  geom = c("point", "text"), habillage=1, label = "var", title = "PCA Seleção Final")#plotando eixos variaveis com os pontos por ecorregiao da ZEE
 
 fviz_pca_biplot(pca.sel.X.CV, habillage = 1, addEllipses = TRUE, title = "PCA Seleção Final", col.var = "blue", label = "var") + scale_color_brewer(palette="Dark2")+ theme_minimal()#plotando com elipses, em acordo com a coluna "Ecoregion" dada por habillage = 1. Manteve boa separacao da selecao minimos e maximos, mas juntou as ecorregioes de Noronha (ER74) e da CVT (ER77)
+
+#---------------------------------------------------
+## Agora uma analise exclusiva para a BC
+
+rodolitosBC = rodolitos3[rodolitos3$Basin=="BC", ]#escolhendo as linhas onde a coluna Basin é igual a BC e mantendo os valores de todas as colunas de rodolitos 3
+rodolitosBC
+head(rodolitosBC)
+
+#PCA para a BC, com batimetria, derivadas batimetria, rios, glodap sup e bio-oracle min e max
+pca.BC.all = PCA(rodolitosBC[,c("BC.region", "Aspect_R", "Aspect_S", "B_CV_Max", "B_CV_Min", "B_Fe_Max", "B_Fe_Min", "B_Light_Max", "B_Light_Min", "B_NIT_Max", "B_NIT_Min", "B_PO4_Max", "B_PO4_Min", "B_SAL_Max", "B_SAL_Min", "B_T_Max", "B_T_Min", "BAT", "FlowDir", "River", "Roughness", "S_ALKA_X", "S_ARA_X", "S_CAL_X", "S_CO2_X", "Slope_R", "Slope_S", "TPI", "TRI")], scale.unit=T, ncp=5, quali.sup=1, graph=T)#explicou 69.2 da variancia dos dado.
+summary(pca.BC.all)
+
+# Contributions of variables to PC1
+fviz_contrib(pca.BC.all, choice = "var", axes = 1, top = 10)#1o eixo dos nutrientes, T, sal e CV
+# Contributions of variables to PC2
+fviz_contrib(pca.BC.all, choice = "var", axes = 2, top = 10)#2o eixo dos demais recursos (luzMin e calcitaX), batimetria, distrios e FerroMax (ref a Iron em cavalcanti et al. 2014 e 2018, McCoy2015, Millar2017, Darrenougue_2013, Dromgoole and Walter 1990)
+# Contributions of variables to PC1 and PC2
+fviz_contrib(pca.BC.all, choice = "var", axes = 1:2, top = 10)
+
+plot.PCA(pca.BC.all, axes=c(1, 2), choix = "ind", habillage=1, label = "var", title = "PCA.BC.all") #plotando os pontos colorindo pela variavel "BC.region" pelo argumento habillage = 1
+
+fviz_pca_biplot(pca.BC.all,  geom = c("point", "text"), habillage=1, label = "var", title = "PCA.BC.all")#plotando eixos variaveis com os pontos por regiao da BC
+
+fviz_pca_biplot(pca.BC.all, habillage = 1, addEllipses = TRUE, title = "PCA.BC.all", col.var = "blue", label = "var") + scale_color_brewer(palette="Dark2")+ theme_minimal()#plotando com elipses, em acordo com a coluna "BC.region" dada por habillage = 1
+
+#Repetindo PCA.BC, mantendo uma variavel entre as que tinham alta correl (retirados fosfato, aragonita, alcalinidade, aspects, TPI e FlowDir)
+pca.BC1 = PCA(rodolitosBC[,c("BC.region", "B_CV_Max", "B_CV_Min", "B_Fe_Max", "B_Light_Max", "B_Light_Min", "B_NIT_Max", "B_NIT_Min", "B_SAL_Max", "B_SAL_Min", "B_T_Max", "B_T_Min", "BAT", "River", "Roughness", "S_CAL_X", "S_CO2_X", "Slope_R", "Slope_S", "TRI")], scale.unit=T, ncp=5, quali.sup=1, graph=T)#explicou 76,3 da variancia dos dados.
+summary(pca.BC1)
+
+# Contributions of variables to PC1
+fviz_contrib(pca.BC1, choice = "var", axes = 1, top = 10)#mantidas as variaveis do PC1 anterior, com aumento do %  de explicação, acrescentando rugosidade e TRI
+# Contributions of variables to PC2
+fviz_contrib(pca.BC1, choice = "var", axes = 2, top = 10)#mantidas as variaveis do PC2 anterior, com aumento do %  de explicação, acrescentando Light_Max
+fviz_contrib(pca.BC1, choice = "var", axes = 1:2, top = 15)
+
+
+plot.PCA(pca.BC1, axes=c(1, 2), choix = "ind", habillage=1, label = "var", title = "PCA BC 1") #plotando os pontos colorindo pela variavel "BC.region" pelo argumento habillage = 1
+
+fviz_pca_biplot(pca.BC1,  geom = c("point", "text"), habillage=1, label = "var", title = "PCA BC 1")#plotando eixos variaveis com os pontos por regiao da BC
+
+fviz_pca_biplot(pca.BC1, habillage = 1, addEllipses = TRUE, title = "PCA BC 1", col.var = "blue", label = "var") + scale_color_brewer(palette="Dark2")+ theme_minimal()#plotando com elipses, em acordo com a coluna "BC.region" dada por habillage = 1
+
+#Repetindo PCA.BC, trocando min e max de T e sal por range e retirando variaveis derivadas da batimetria devido alta correl entre ela e NIT_Max
+
+colnames(rodolitosBC)
+
+pca.BC2 = PCA(rodolitosBC[,c("BC.region", "B_CV_Max", "B_CV_Min", "B_Fe_Max", "B_Light_Max", "B_Light_Min", "B_NIT_Max", "B_NIT_Min", "B_SAL_R", "B_T_R", "BAT", "River", "S_CAL_X", "S_CO2_X")], scale.unit=T, ncp=5, quali.sup=1, graph=T)#aumentou a explicação para 79,74 da variancia dos dados.
+summary(pca.BC2)
+
+# Contributions of variables to PC1
+fviz_contrib(pca.BC2, choice = "var", axes = 1, top = 10)#mantidas as variaveis do PC1 all (nutrientes, T, sal e CV)
+# Contributions of variables to PC2
+fviz_contrib(pca.BC2, choice = "var", axes = 2, top = 10)#mantidas as variaveis do PC2 anterior, com aumento do %  de explicação
+fviz_contrib(pca.BC2, choice = "var", axes = 1:2, top = 15)
+
+
+plot.PCA(pca.BC2, axes=c(1, 2), choix = "ind", habillage=1, label = "var", title = "PCA BC 2") #plotando os pontos colorindo pela variavel "BC.region" pelo argumento habillage = 1
+
+fviz_pca_biplot(pca.BC2,  geom = c("point", "text"), habillage=1, label = "var", title = "PCA BC 2")#plotando eixos variaveis com os pontos por regiao da BC
+
+fviz_pca_biplot(pca.BC2, habillage = 1, addEllipses = TRUE, title = "PCA BC 2", col.var = "blue", label = "var") + scale_color_brewer(palette="Dark2")+ theme_minimal()#plotando com elipses, em acordo com a coluna "BC.region" dada por habillage = 1
+
+#Repetindo PCA.BC, trocando min e max de NIT por range e retirando CV_Min devido alta correl positiva com T e Sal
+
+colnames(rodolitosBC)
+
+pca.BC3 = PCA(rodolitosBC[,c("BC.region", "B_CV_Max", "B_Fe_Max", "B_Light_Max", "B_Light_Min", "B_NIT_R", "B_SAL_R", "B_T_R", "BAT", "River", "S_CAL_X", "S_CO2_X")], scale.unit=T, ncp=5, quali.sup=1, graph=T)#reduziu a explicação para 75,54 da variancia dos dados.
+summary(pca.BC3)
+
+# Contributions of variables to PC1
+fviz_contrib(pca.BC3, choice = "var", axes = 1, top = 10)#inverteu a lógica dos eixos, PC1 com nitrato_range + as IPC dos PC2 anteriores (calcita, CO2, batimetria, Fe e LuzMin)
+# Contributions of variables to PC2
+fviz_contrib(pca.BC3, choice = "var", axes = 2, top = 10)#com inversão as IPC foram T, sal, CV_max e luzMax
+fviz_contrib(pca.BC3, choice = "var", axes = 1:2, top = 15)
+
+
+plot.PCA(pca.BC3, axes=c(1, 2), choix = "ind", habillage=1, label = "var", title = "PCA BC 3") #plotando os pontos colorindo pela variavel "BC.region" pelo argumento habillage = 1
+
+fviz_pca_biplot(pca.BC3,  geom = c("point", "text"), habillage=1, label = "var", title = "PCA BC 3")#plotando eixos variaveis com os pontos por regiao da BC
+
+fviz_pca_biplot(pca.BC3, habillage = 1, addEllipses = TRUE, title = "PCA BC 3", col.var = "blue", label = "var") + scale_color_brewer(palette="Dark2")+ theme_minimal()#plotando com elipses, em acordo com a coluna "BC.region" dada por habillage = 1
+
+#Repetindo PCA.BC, deixando NIT_Max e Light_Min e tirando os opostos
+
+colnames(rodolitosBC)
+
+pca.BC4 = PCA(rodolitosBC[,c("BC.region", "B_CV_Max", "B_Fe_Max", "B_Light_Min", "B_NIT_Max", "B_SAL_R", "B_T_R", "BAT", "River", "S_CAL_X", "S_CO2_X")], scale.unit=T, ncp=5, quali.sup=1, graph=T)#maior explicação 81,44 da variancia dos dados.
+summary(pca.BC4)
+
+# Contributions of variables to PC1
+fviz_contrib(pca.BC4, choice = "var", axes = 1, top = 10)#mantida a lógica invertida dos eixos, PC1 com nitrato + as IPC dos PC2 anteriores (CO2, batimetria, Fe e rio no lugar de LuzMin), exceto calcita
+# Contributions of variables to PC2
+fviz_contrib(pca.BC4, choice = "var", axes = 2, top = 10)#com inversão as IPC foram T, sal, CV_max e calcita (nitrato_max e luz_min estranhamente entraram aqui tb)
+fviz_contrib(pca.BC4, choice = "var", axes = 1:2, top = 15)
+
+
+plot.PCA(pca.BC4, axes=c(1, 2), choix = "ind", habillage=1, label = "var", title = "PCA BC 4") #plotando os pontos colorindo pela variavel "BC.region" pelo argumento habillage = 1
+
+fviz_pca_biplot(pca.BC4,  geom = c("point", "text"), habillage=1, label = "var", title = "PCA BC 4")#plotando eixos variaveis com os pontos por regiao da BC
+
+fviz_pca_biplot(pca.BC4, habillage = 1, addEllipses = TRUE, title = "PCA BC 4", col.var = "blue", label = "var") + scale_color_brewer(palette="Dark2")+ theme_minimal()#plotando com elipses, em acordo com a coluna "BC.region" dada por habillage = 1
+
+##em todos os testes os setores da BC ficaram bem separados com somente 2 outliers do setor norte junto ao setor sul. INVESTIGAR
+#Batimetria está com valores absolutos negativos, portanto a leitura é invertida. Retirar o sinal negativo da planilha-base. 
